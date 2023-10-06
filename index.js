77f3c16ef48c6dc62db7868cbcdda5edba03c82b
Divya R <kwrdivyaraj100@gmail.com>
2023-10-06 11:38:01 +0530

    adding register form using bootstrap

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
 
-dotenv.config({path: './.env'});
+dotenv.config({ path: './.env' });
 const app = express();
 const db = mysql.createConnection({
-    host:process.env.DATABASE_HOST,
-    user:process.env.DATABASE_USER,
-    password:process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 })
 const publicDirectory = path.join(__dirname, './public');
@@ -16,21 +16,26 @@ app.use(express.static(publicDirectory));
 
 app.set('view engine', 'hbs')
 
-db.connect((err)=>{
-    if(err){
+db.connect((err) => {
+    if (err) {
         console.log();
-    }else{
+    } else {
         console.log("MySQL Connected");
     }
 })
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     // res.send("<h1>Home Page</h1>")
     res.render("index");
+});
+
+app.get("/register", (req, res) => {
+    // res.send("<h1>Home Page</h1>")
+    res.render("register");
 })
 
 
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("Server Started on port 5000");
 })
